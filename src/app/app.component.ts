af2a0d5aacb27e7f24d6012321d398aa6fa48f03
mbijou <mimounbijjou@Mimouns-Air.local>
2020-11-02 00:30:50 +0100

    added sb-ui-kit theme and static content for open hours, phone number and location

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
+import * as feather from 'feather-icons';
+import * as $ from 'jquery';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'home';
+
+  ngOnInit(): void{
+    AOS.init(
+      {
+        disable: 'mobile',
+        duration: 600,
+        once: true,
+        startEvent: 'load'
+      }
+    );
+    window.addEventListener('load', AOS.refresh);
+    feather.replace();
+  }
 }
